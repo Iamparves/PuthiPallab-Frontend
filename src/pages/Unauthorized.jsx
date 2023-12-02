b6f8eb05dc8b1e2f3977e12ea921464f22c59010
Parves Hossain Rabby <parves.pe.wb.h@gmail.com>
2023-12-02 22:50:47 +0600

    unauthorized page

--- src/pages/Unauthorized.jsx
+++ src/pages/Unauthorized.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdKeyboardBackspace } from "react-icons/md";
+import { Link, useNavigate } from "react-router-dom";
 
 const Unauthorized = () => {
   const navigate = useNavigate();
@@ -7,10 +9,38 @@ const Unauthorized = () => {
 
   return (
     <main>
-      <section>
-        <h1>Unauthorized</h1>
-        <p>You do not have permission to access the requested page.</p>
-        <button onClick={goBack}>Go Back</button>
+      <section className="flex min-h-[100dvh] items-center justify-center bg-white">
+        <div className="container">
+          <div className="mx-auto grid max-w-sm grid-cols-1 items-center gap-10 rounded-md md:max-w-3xl md:grid-cols-[auto_1fr]">
+            <img
+              className="mx-auto block w-[300px] lg:w-[350px]"
+              src="/401-error.png"
+            />
+            <div className="text-center md:text-left">
+              <h1 className="mb-2 text-2xl font-semibold text-primary md:text-3xl">
+                Unauthorized
+              </h1>
+              <p className="mb-5 text-gray-500 md:text-lg">
+                You do not have permission to access the requested page.
+              </p>
+              <div className="flex items-center justify-center gap-2 md:justify-start">
+                <button
+                  className="flex items-center gap-1 rounded-full border-2 border-primary bg-primary px-5 py-2.5 font-medium text-white duration-300 hover:bg-white hover:text-primary"
+                  onClick={goBack}
+                >
+                  <MdKeyboardBackspace />
+                  Go Back
+                </button>
+                <Link
+                  className="flex items-center gap-1 rounded-full border-2 border-primary px-5 py-2.5 text-primary duration-300 hover:bg-primary hover:text-white"
+                  to="/"
+                >
+                  <IoHomeOutline /> Home
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
