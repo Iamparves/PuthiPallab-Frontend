aede67c4ac6041d1dc2e6892ee946be8772c754d
Parves Hossain Rabby <parves.pe.wb.h@gmail.com>
2023-12-07 01:33:07 +0600

    dummy hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,24 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <main className="h-[85vh]">
-      <div className="space-x-2 pt-10 text-center">
-        <Link
-          to="/login"
-          className="mt-6 inline-block rounded-lg bg-primary px-10 py-4 text-center font-semibold text-white"
-        >
-          Log In
-        </Link>
-        <Link
-          to="/dashboard"
-          className="mt-6 inline-block rounded-lg bg-primary px-10 py-4 text-center font-semibold text-white"
-        >
-          Dashboard
-        </Link>
-      </div>
+    <main>
+      <section className="bg-[url(/img-1.jpg)] bg-cover bg-center bg-no-repeat">
+        <div className="flex min-h-[calc(100svh-64px)] w-full flex-col justify-center bg-black/60 md:min-h-[calc(100svh-80px)]">
+          <div className="container flex flex-col items-center text-center">
+            <h1 className="mb-3 text-3xl font-bold text-white md:text-5xl md:leading-tight">
+              Journey into the{" "}
+              <span className="text-primary">World of Books</span>
+            </h1>
+            <p className="mb-5 text-lg text-gray-100 md:text-2xl">
+              Explore the Boundless Universe of Knowledge Through the Pages of
+              Puthi Pallab
+            </p>
+            <Link className="mx-auto block rounded-full border-2 border-primary bg-primary px-6 py-3 text-center font-semibold text-white duration-300 hover:bg-white hover:text-primary">
+              Know more
+            </Link>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
