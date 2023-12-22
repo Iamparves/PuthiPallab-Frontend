0c70a0c3b49d80fe67f1690452c1de1a3a5a943b
Parves Hossain Rabby <parves.pe.wb.h@gmail.com>
2023-12-22 11:58:05 +0600

    contact page complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-white py-5 shadow-[0_-1px_2px_rgba(0,0,0,0.05)]">
+    <footer className="bg-white py-5 shadow-[0_-1px_3px_rgba(0,0,0,0.05)]">
       <div className="container text-center">
-        <p className="text-xs text-gray-400 sm:text-sm">
+        <p className="text-xs text-[#999] sm:text-sm">
           &copy; {new Date().getFullYear()} Puthi Pallab Library. All Rights
           Reserved.
         </p>
