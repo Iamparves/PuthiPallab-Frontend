1fe07ec0d31b5fdb7b9ac7446fc36d4dd8c7b2b3
Parves Hossain Rabby <parves.pe.wb.h@gmail.com>
2023-12-27 17:34:00 +0600

    fix mobile hover problem

--- src/components/FilterCheckBox.jsx
+++ src/components/FilterCheckBox.jsx
@@ -16,7 +16,7 @@ const FilterCheckBox = ({ value, handler, list, genre }) => {
       <span className="flex aspect-square h-4 items-center justify-center rounded-[4px] border border-[#e1e1e1] text-xs text-white duration-300 lg:group-hover:text-primary">
         <GiCheckMark />
       </span>
-      <span className="text-sm leading-[1.3] text-[#1d1d1d] duration-300 group-hover:text-primary">
+      <span className="text-sm leading-[1.3] text-[#1d1d1d] duration-300 lg:group-hover:text-primary">
         {genre || value}
       </span>
     </label>
