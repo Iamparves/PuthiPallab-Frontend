026747e9330a65c9550264fdd26506101ed90531
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2024-01-14 21:04:30 -0300

    feat: add get all documents api call

--- src/utils/firebaseRequest.js
+++ src/utils/firebaseRequest.js
@@ -1,7 +1,12 @@
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc, getDocs  } from "firebase/firestore"; 
 import { db } from "./firebaseSetup";
 
 export const AddDocument = async (users, data) => {
   const docRef = await addDoc(collection(db, users), data);
   return docRef;
+}
+
+export const GetAllDocuments = async (collectionName) => {
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  return querySnapshot;
 }
